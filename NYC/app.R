53878781c8d80e5bc441c3dfaeaa132b3010f2db
Jinfei Xue <xuejinfei@Jinfeis-MacBook-Pro.local>
2019-05-01 20:05:40 -0400

    fiona and aeris

--- NYC/app.R
+++ NYC/app.R
@@ -416,24 +416,18 @@ ui <- dashboardPage(
             tabItem(
                 tabName = "facility",
                  fluidRow(
-                 
-                    
-                    box( title = "Controls", status = "warning", solidHeader = TRUE,
-                         selectInput("boro1","Select community to view on left side panel:",cmd,selected = cmd[1]),
-                         br(),
-                         br(),
-                         br(),
-                         selectInput("boro2","Select community to view right side panel:" ,cmd,selected = cmd[3])),
-                    box(title = "Controls",status = "warning", solidHeader = TRUE,
-                         checkboxGroupInput("boro11","Select facility type to view:",choices = factype ,selected = factype)
-                        )
-                    ),
-              
-                         
-                    
-                 fluidRow(
-                  column(width = 12,
-                           box(  status = "primary", solidHeader = FALSE,background = "teal",
+                   column(width = 3,
+                          box( title = "Left Map Control", width = NULL, status = "primary",
+                               selectInput("boro1","Select community to view on left side panel:",cmd,selected = cmd[1])),
+                          box(title = "Right Map Control", width = NULL, status = "primary",
+                              selectInput("boro2","Select community to view right side panel:" ,cmd,selected = cmd[3])),
+                          box(title = "Controls", width = NULL, status = "primary",
+                              checkboxGroupInput("boro11","Select facility type to view:",choices = factype ,selected = factype)
+                          )
+                          )     
+                    ,
+                  column(width = 9,
+                           box( 
                                 
                                 leafletOutput("facility_1",height = 500),
                                 br(),
@@ -449,7 +443,7 @@ ui <- dashboardPage(
                                 valueBoxOutput("progressBox6"))     
                                       
                            
-                           ,box(status = "primary", solidHeader = FALSE,background = "olive",
+                           ,box(
                                 leafletOutput("facility_2",height = 500),
                                 br(),
                                 br(),
@@ -464,10 +458,8 @@ ui <- dashboardPage(
                                 valueBoxOutput("progressBox66")   
                            )
                     
-                           
-                           
-                    )
                 )
+            )
             ),
             tabItem(
                 tabName = "DT",
@@ -494,7 +486,7 @@ ui <- dashboardPage(
                 br(),
                 h4("Contact information"),
                 h4("Instructor: Eric Kolaczyk"),
-                h4("This shiny app developed by Mark Yan, Samuel Luo, Si Chen, Sharry Zhang"),
+                h4("abcd"),
                 h4("Contact us at mssp@bu.edu")
             )
         )
